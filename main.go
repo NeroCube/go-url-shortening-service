1ed457c4edb75a365f2efc4d80ca925374bb52d6
陳冠廷 <join810316@gmail.com>
2018-04-20 01:21:12 +0800

    set server port with config

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"./config"
 )
 
 func main() {
 
+	// Read config
+	config, err := config.FromDefault()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(config.Server.Port, router))
 }
