39a60b9260fc4974e3a906f4a65f2e0c84d35588
陳冠廷 <join810316@gmail.com>
2018-05-02 00:20:09 +0800

    fix the border error

--- encode/encode.go
+++ encode/encode.go
@@ -12,7 +12,7 @@ func TinyURL(random_length int) string {
 	var charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	var b bytes.Buffer
 
-	for i := 0; i <= random_length; i++ {
+	for i := 0; i < random_length; i++ {
 		rand.Seed(time.Now().UnixNano())
 		// the length of charSet is 62
 		b.WriteString(fmt.Sprintf("%v", string(charSet[rand.Intn(62)])))
