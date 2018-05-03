adc318322ceaba27348b71ac46abb8571bc1bbc3
陳冠廷 <join810316@gmail.com>
2018-05-03 22:44:25 +0800

    add set, get, incr, decr func

--- redis/redis.go
+++ redis/redis.go
@@ -1,24 +1,50 @@
 package redis
 
 import (
-	"fmt"
+	"time"
 
 	"github.com/go-redis/redis"
 )
 
-func init() {
-	New()
-	fmt.Println("init")
-}
+var connect = New()
 
-func New() {
+func New() redis.Client {
 	client := redis.NewClient(&redis.Options{
 		Addr:     "app_redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
-	// Output: PONG <nil>
+	return *client
+}
+
+func Set(key string, value string, expiration_second int32) {
+	err := connect.Set(key, value, time.Duration(expiration_second)*time.Second).Err()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Get(key string) string {
+	val, err := connect.Get(key).Result()
+	if err != nil {
+		panic(err)
+	}
+	return val
+}
+
+func Incr(key string) int64 {
+	result, err := connect.Incr(key).Result()
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func Decr(key string) int64 {
+	result, err := connect.Decr(key).Result()
+	if err != nil {
+		panic(err)
+	}
+	return result
 }
