3c7856214d4be68afcef3cf4897f5c9f61cb1beb
陳冠廷 <join810316@gmail.com>
2018-05-11 02:04:22 +0800

    add info func

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -1,20 +1,21 @@
 package postgres
 
 import (
-	"database/sql"
 	"fmt"
 	"os"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
 
-func Connect() {
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
-	db, err := sql.Open("postgres", dbinfo)
-	fmt.Println(dbinfo)
+func Info() string {
+	port, err := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
 	checkErr(err)
-	defer db.Close()
+
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%d sslmode=disable",
+		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"), os.Getenv("POSTGRES_HOST"), port)
+
+	return dbinfo
 }
 func checkErr(err error) {
 	if err != nil {
