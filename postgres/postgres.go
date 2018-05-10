5b6b6d015e6a2154bc612a4bdeda79a0486d7325
陳冠廷 <join810316@gmail.com>
2018-05-11 00:19:50 +0800

    remove const

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -8,15 +8,9 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	DB_USER     := os.Getenv("POSTGRES_USER")
-	DB_PASSWORD := os.Getenv("POSTGRES_PASSWORD")
-	DB_NAME     := os.Getenv("POSTGRES_DB")
-)
-
 func Connect() {
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		DB_USER, DB_PASSWORD, DB_NAME)
+		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
 	db, err := sql.Open("postgres", dbinfo)
 	fmt.Println(dbinfo)
 	checkErr(err)
