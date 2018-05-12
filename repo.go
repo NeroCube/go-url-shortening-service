1eb80a70f052dbb77723e5cb32882c2f3ac26895
Nero <nerocube.tw@gmail.com>
2018-05-13 01:16:24 +0800

    add insert func

--- repo.go
+++ repo.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"time"
 
+	_ "github.com/lib/pq"
 	"github.com/nerocube/go-url-shortening-service/encode"
 	"github.com/nerocube/go-url-shortening-service/postgres"
 	"github.com/nerocube/go-url-shortening-service/redis"
@@ -14,9 +16,6 @@ var urlmaps URLMaps
 // Give us some seed data
 func init() {
 	redis.Set("counter", "0", 0)
-	postgres.Connect()
-	RepoCreateURLMap(URLMap{OriginalURL: "https://github.com"})
-	RepoCreateURLMap(URLMap{OriginalURL: "https://google.com/"})
 }
 
 func RepoFindURLMap(id int) URLMap {
@@ -43,6 +42,7 @@ func RepoCreateURLMap(t URLMap) URLMap {
 	t.ShortenURL = TinyURL
 	t.ID = int(redis.Incr("counter"))
 	t.Created = time.Now()
+	InsertURLMap(t)
 	urlmaps = append(urlmaps, t)
 	return t
 }
@@ -55,4 +55,21 @@ func RepoDestroyURLMap(id int) error {
 		}
 	}
 	return fmt.Errorf("Could not find URLMap with id of %d to delete", id)
+}
+
+func InsertURLMap(t URLMap) {
+	dbinfo := postgres.Info()
+	db, err := sql.Open("postgres", dbinfo)
+	checkErr(err)
+	stmt, err := db.Prepare("INSERT INTO public.url_map( original_url, tiny_url, created_at) VALUES ($1, $2, $3);")
+	checkErr(err)
+	_, err = stmt.Exec(t.OriginalURL, t.ShortenURL, t.Created)
+	checkErr(err)
+	defer db.Close()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
