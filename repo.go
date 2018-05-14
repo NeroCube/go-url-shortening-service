3fc4de3c82e943d7fa3f4bde0b4cdf6d49afbbf1
Nero <nerocube.tw@gmail.com>
2018-05-15 01:38:12 +0800

    add the logic of find OriginalURL and db operator

--- repo.go
+++ repo.go
@@ -28,19 +28,18 @@ func RepoFindURLMap(id int) URLMap {
 	return URLMap{}
 }
 
-//this is bad, I don't think it passes race conditions
 func RepoCreateURLMap(t URLMap) URLMap {
-	TinyURL := ""
+	TinyURL := SelectTinyURL(t.OriginalURL)
+
 	for {
 		TinyURL = encode.TinyURL(6)
-		isExists := redis.Exists(TinyURL)
-		if !isExists {
+		if !IsExistsTinyURL(TinyURL) {
 			break
 		}
 	}
+
 	redis.Set(TinyURL, t.OriginalURL, 0)
 	t.ShortenURL = TinyURL
-	t.ID = int(redis.Incr("counter"))
 	t.Created = time.Now()
 	InsertURLMap(t)
 	urlmaps = append(urlmaps, t)
@@ -57,15 +56,64 @@ func RepoDestroyURLMap(id int) error {
 	return fmt.Errorf("Could not find URLMap with id of %d to delete", id)
 }
 
+func RepoFindOriginalURL(tiny_url string) string {
+	if redis.Exists(tiny_url) {
+		return redis.Get(tiny_url)
+	}
+
+	if IsExistsTinyURL(tiny_url) {
+		return SelectOriginalURL(tiny_url)
+	}
+
+	return ""
+}
+
 func InsertURLMap(t URLMap) {
 	dbinfo := postgres.Info()
 	db, err := sql.Open("postgres", dbinfo)
 	checkErr(err)
+
 	stmt, err := db.Prepare("INSERT INTO public.url_map( original_url, tiny_url, created_at) VALUES ($1, $2, $3);")
 	checkErr(err)
+
 	_, err = stmt.Exec(t.OriginalURL, t.ShortenURL, t.Created)
 	checkErr(err)
+
 	defer db.Close()
+}
+
+func SelectOriginalURL(TinyURL string) string {
+	dbinfo := postgres.Info()
+	db, err := sql.Open("postgres", dbinfo)
+	checkErr(err)
+
+	original_url := ""
+	sqlStatement := "SELECT original_url FROM public.url_map WHERE tiny_url = $1;"
+	err = db.QueryRow(sqlStatement, TinyURL).Scan(&original_url)
+	checkErr(err)
+
+	return original_url
+}
+
+func SelectTinyURL(OriginalURL string) string {
+	dbinfo := postgres.Info()
+	db, err := sql.Open("postgres", dbinfo)
+	checkErr(err)
+
+	tiny_url := ""
+	sqlStatement := "SELECT tiny_url FROM public.url_map WHERE original_url = $1;"
+	err = db.QueryRow(sqlStatement, OriginalURL).Scan(&tiny_url)
+	checkErr(err)
+
+	return tiny_url
+}
+
+func IsExistsTinyURL(TinyURL string) bool {
+	if SelectOriginalURL(TinyURL) == "" {
+		return false
+	}
+
+	return true
 }
 
 func checkErr(err error) {
